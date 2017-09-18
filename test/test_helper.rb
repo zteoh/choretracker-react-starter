1cb61f46913b61218dad8a3ce447005b14deca98
Rebecca Kern <rkern@andrew.cmu.edu>
2017-09-18 09:56:53 -0400

    added some additional things to test helper structure

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,16 @@
-require 'simplecov'
-SimpleCov.start 'rails'
+# TODO: SimpleCov required here...
+
+
+
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'turn/autorun'
-require 'contexts'
+# TODO: Turn required here...
+
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
+
   # include the Contexts module for all tests
   include Contexts
 
@@ -16,7 +19,13 @@ class ActiveSupport::TestCase
     assert !condition, msg
   end
 
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
 end
 
-# Formatting test output a litte nicer
-Turn.config.format = :outline
+# Formatting test output a litte nicer ...
